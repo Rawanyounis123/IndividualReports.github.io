6e4fa8a80eaf8ce70d90bf4faa222512f17900a6
Rawan Younis <41347412+Rawanyounis123@users.noreply.github.com>
2023-05-01 01:02:33 +0300

    Few Edits

--- generate_report.py
+++ generate_report.py
@@ -1,5 +1,7 @@
 from io import BytesIO
-from flask import Flask
+from flask import Flask, request, send_file
+import mcq_analyzer 
+app = Flask(__name__)
 
 @app.route('/generate', methods=['GET'])
 def generate():
@@ -7,11 +9,9 @@ def generate():
     student_answers_file = request.files['student_answers_file']
     report_file = BytesIO()
     
-
     # Generate the report using the mcq_analyzer module
     report = mcq_analyzer.generate_report(answer_key_file, student_answers_file, report_file)
 
-   
     # Return the report file to the user for download
     report_file.seek(0)
-    return send_file(report_file, as_attachment=True, attachment_filename=report_file)
+    return send_file(report_file, as_attachment=True, attachment_filename='test_report.docx')
