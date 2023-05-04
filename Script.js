ca3fc28447a22dcfbd3e146365761dc8a0e04ad8
Rawan Younis <41347412+Rawanyounis123@users.noreply.github.com>
2023-05-04 20:15:50 +0300

    Javascript

--- Script.js
+++ Script.js
@@ -1,39 +1,44 @@
-// Get references to the form inputs
-const answerKeyInput = document.querySelector('#answer-key');
-const studentAnswerInput = document.querySelector('#student-answer');
 
-// Set up an event listener for the form submission
+
+
 document.querySelector('#report-form').addEventListener('submit', (event) => {
   // Prevent the form from submitting and refreshing the page
   event.preventDefault();
 
   // Generate the report document using the answer key and student answer
-  const doc = generateReport();
-  // Save the report document as a file and offer it for download
-  const blob = new Blob([doc], {type: 'application/pdf'});
+  const pdfContent = generateReport();
+
+  // Create a Blob from the PDF content
+  const blob = new Blob([pdfContent], {type: 'application/pdf'});
+
+  // Create a temporary URL to the Blob
   const url = URL.createObjectURL(blob);
+
+  // Create a link element to download the PDF
   const a = document.createElement('a');
   a.href = url;
-  a.download = 'report.doc';
+  a.download = 'report.pdf';
   document.body.appendChild(a);
+
+  // Trigger a click on the link element to start the download
   a.click();
+
+  // Remove the link element from the document
   document.body.removeChild(a);
+
+  // Revoke the URL to free up memory
   URL.revokeObjectURL(url);
-    
 });
 
 
-
 function generateReport() {
-      // Create a new PDF document
+  
+  // Create a new PDF document
   const doc = new jsPDF();
 
   // Add some content to the document
   doc.text('Hello World!', 10, 10);
 
-  // Save the PDF document
-  doc.save('output.pdf');
-  
-  return doc;
+  // Return the content of the PDF document as a string
+  return doc.output('datauristring');
 }
-  
